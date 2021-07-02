d90e61d9ac77fb45c8dd1ea80e18b627dc6f0b6a
y <y@DESKTOP-1E4JF9F>
2021-07-02 13:31:59 +0900

    IJ : mybatis / Spring 세팅작업

---
+++ WebContent/WEB-INF/view/main/main.jsp
@@ -0,0 +1,6 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+
+<h1>GIT 사용</h1>
+<h1>이호재입니다.</h1>
---
+++ src/bean/main/main.java
@@ -0,0 +1,14 @@
+package bean.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/health/")
+public class main {
+	
+	@RequestMapping("main.go")
+	public String front() {
+		return "/main/main";
+	}
+}
