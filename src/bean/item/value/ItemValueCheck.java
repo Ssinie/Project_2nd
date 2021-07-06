b17fd37a30109a6609d02f79d8a436e6114c3330
username <y@DESKTOP-1E4JF9F>
2021-07-06 18:24:04 +0900

    IJ : 사이트 크롤링 할 URL 추가

--- src/bean/item/value/ItemValueCheck.java
+++ src/bean/item/value/ItemValueCheck.java
@@ -15,6 +15,12 @@ public class ItemValueCheck {
 	@Autowired
 	private SqlSessionTemplate dao = null;
 	
+	
+	// 옥션 search URL : https://www.coupang.com/np/search?component=&q=몬스터머슬WPH-2kg+헬스보충제+순수근육/순수단백질
+	// 쿠팡 search URL : https://browse.auction.co.kr/search?keyword=몬스터머슬WPH-2kg+헬스보충제
+	
+	
+	
 	@RequestMapping("itemcheck.go")
 	public String ItemValueCheck(Model model) {
 		
@@ -28,5 +34,13 @@ public class ItemValueCheck {
 		
 		model.addAttribute("list", list);
 		return "/master/ItemValueCheck";
+	}
+	
+	
+	// 옥션, 쿠팡에서 'item_name'에서 가져온 이름을 이용해 검색 후 최저가 5개의 가격 정보를 가져온다..
+	public void ItemValueSearch() {
+		String aucpath = "https://www.coupang.com/np/search?component=&q=";
+		String coupath = "https://browse.auction.co.kr/search?keyword=";
+		
 	}
 }
