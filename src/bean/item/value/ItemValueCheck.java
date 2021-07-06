bcd38e89cdeb64d3f9f93f4f554c823461770f99
username <y@DESKTOP-1E4JF9F>
2021-07-06 14:17:20 +0900

    IJ : Ajox 사용을 위한 js폴더 추가 / SQL문 추가 및 일부 페이지 처리 작업..

--- src/bean/item/value/ItemValueCheck.java
+++ src/bean/item/value/ItemValueCheck.java
@@ -1,5 +1,8 @@
 package bean.item.value;
 
+import java.util.List;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,8 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/master/")
 public class ItemValueCheck {
 	
+	@Autowired
+	private SqlSessionTemplate dao = null;
+	
 	public String ItemValueCheck() {
 		
+		List member = dao.selectList("Item_value.getItemName");
 		
 		
 		return "master/ItemValueCheck";
---
+++ src/bean/item/value/ItemValueDTO.java
@@ -0,0 +1,21 @@
+package bean.item.value;
+
+public class ItemValueDTO {
+
+	private int num;
+	private String name;
+	
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
