690eb13851e013a82f65b000aeb3e21ffabb510a
dlgo <dlgo@localhost>
2021-07-14 16:06:06 +0900

    hello-contorl.xml / config.xml 수정 및 회원정보 작업 추가

---
+++ src/bean/member/MemberDTO.java
@@ -0,0 +1,86 @@
+package bean.member;
+
+import java.util.Date;
+
+public class MemberDTO {
+
+	private String id;
+	private String nickname;
+	private String profile_image;
+	private String email;
+	private String gender;
+	private String age;
+	private String birthday;
+	private String type;
+	private Date signup;
+	private Date last_login;
+	private String status;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getProfile_image() {
+		return profile_image;
+	}
+	public void setProfile_image(String profile_image) {
+		this.profile_image = profile_image;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Date getSignup() {
+		return signup;
+	}
+	public void setSignup(Date signup) {
+		this.signup = signup;
+	}
+	public Date getLast_login() {
+		return last_login;
+	}
+	public void setLast_login(Date last_login) {
+		this.last_login = last_login;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+}
