4472b12fa7cff88c97f0896947dc34ced3c935c6
ssinie <ssinie1127@gmail.com>
2021-07-15 15:50:23 +0900

    IJ : DTO 추가 및 DB 삽입 전 메서드 작업 완료

--- src/bean/item/name/ItemNameCheck.java
+++ src/bean/item/name/ItemNameCheck.java
@@ -1,6 +1,7 @@
 package bean.item.name;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.rosuda.REngine.REXP;
@@ -17,8 +18,29 @@ public class ItemNameCheck {
 	public static RConnection conn= null;
 	
 	public static void main(String[] args) throws Exception{
-		long catId = 50002426;
-		ItemNameCheck check = new ItemNameCheck(); 
+		ItemNameCheck check = new ItemNameCheck();
+		ItemNameDTO dto = null;
+		int [] catIds = check.marketnum();
+		
+		for(int v : catIds) {
+			HashMap markettag = check.marktetag(v);
+			String maintag = (String)markettag.get("maintag");
+			String subtag = (String)markettag.get("subtag");
+			long catId = 50000000 + v;
+			RList list = check.itemreward(catId);
+			if(list != null) {
+				for(int i = 0; i < list.size(); i++) {
+					String [] val = list.at(i).asStrings();
+					dto = new ItemNameDTO();
+					dto.setName(val[0]);
+					dto.setUrl(val[1]);
+					dto.setMaintag(maintag);
+					dto.setSubtag(subtag);
+				}
+			}
+		}
+		/*
+		 
 		System.out.println("처리시작");
 		RList list = check.itemreward(catId);
 		
@@ -32,7 +54,7 @@ public class ItemNameCheck {
 		}
 		
 		System.out.println("처리종료");
-		
+		*/
 	}
 	
 	// 상품의 catId와 상품명의 갯수를 이용한 상품의 상품명, URL을 가져오는 메서드
@@ -103,5 +125,46 @@ public class ItemNameCheck {
 			conn.close();
 		}
 		return pageNum;
+	}
+	
+	// 마켓 카테고리를 list로 리턴해줌..
+	public int [] marketnum() {
+		int [] listnum = {2425, 2426, 2427, 2428, 2429, 7042, 7043, 7044, 2440, 2441, 2442, 
+							2443, 2444, 2445, 2446, 2447, 2448, 2608, 2609, 2610, 2612};			
+		return listnum;
+	}
+	
+	// 마켓 카테고리를 비교하여 maintag, subtag를 뽑아내기위한 메서드
+	public HashMap marktetag(int catId) {
+		HashMap hash = new HashMap();
+		String maintag = "";
+		String subtag = "";
+		switch(catId){
+			case 2425 :	maintag = "비타민";subtag = "멀티비타민";break;
+			case 2426 :	maintag = "비타민";subtag = "비타민A";break;
+			case 2427 :	maintag = "비타민";subtag = "비타민B";break;
+			case 2428 :	maintag = "비타민";subtag = "비타민C";break;
+			case 2429 :	maintag = "비타민";subtag = "기타비타민";break;
+			case 7042 :	maintag = "비타민";subtag = "비타민D";break;
+			case 7043 :	maintag = "비타민";subtag = "비타민E";break;
+			case 7044 :	maintag = "비타민";subtag = "비오틴";break;
+				
+			case 2440 :	maintag = "영양제";subtag = "엽산";break;
+			case 2441 :	maintag = "영양제";subtag = "아연";break;
+			case 2442 :	maintag = "영양제";subtag = "철분";break;
+			case 2443 :	maintag = "영양제";subtag = "칼슘";break;
+			case 2444 :	maintag = "영양제";subtag = "마그네슘";	break;
+			case 2445 :	maintag = "영양제";subtag = "프로폴리스";break;
+			case 2446 :	maintag = "영양제";subtag = "글루코사민";break;
+			case 2447 :	maintag = "영양제";subtag = "오메가3";break;
+			case 2448 :	maintag = "영양제";subtag = "감마리놀렌산";break;
+			case 2608 :	maintag = "영양제";subtag = "루테인";break;
+			case 2609 :	maintag = "영양제";subtag = "스쿠알렌";break;
+			case 2610 :	maintag = "영양제";subtag = "쏘팔메토";break;
+			case 2612 :	maintag = "영양제";subtag = "키토산";break;
+		}
+		hash.put("maintag", maintag);
+		hash.put("subtag", subtag);
+		return hash;
 	}
 }
---
+++ src/bean/item/name/ItemNameDTO.java
@@ -0,0 +1,47 @@
+package bean.item.name;
+
+public class ItemNameDTO {
+	private int num;
+	private String name;
+	private String url;
+	private String maintag;
+	private String subtag;
+	private String imgurl;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getMaintag() {
+		return maintag;
+	}
+	public void setMaintag(String maintag) {
+		this.maintag = maintag;
+	}
+	public String getSubtag() {
+		return subtag;
+	}
+	public void setSubtag(String subtag) {
+		this.subtag = subtag;
+	}
+	public String getImgurl() {
+		return imgurl;
+	}
+	public void setImgurl(String imgurl) {
+		this.imgurl = imgurl;
+	}
+}
