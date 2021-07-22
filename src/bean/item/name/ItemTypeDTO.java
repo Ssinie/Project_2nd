6d62154debfa80674c6d1fab049d0d8f32c10bb9
ssinie <ssinie1127@gmail.com>
2021-07-22 16:27:19 +0900

    IJ : 제품정보를 가져오기위한 DTO 생성 / Spring, mybatis 설정 업로드

---
+++ src/bean/item/name/ItemTypeDTO.java
@@ -0,0 +1,140 @@
+package bean.item.name;
+
+public class ItemTypeDTO {
+
+	
+	/*
+	 * LCNS_NO	인허가번호
+	 * PRMS_DT	보고일자
+	 * PRDLST_REPORT_NO	품목제조번호
+	 * CRET_DTM	최초생성일시
+	 * LAST_UPDT_DTM	최종수정일시
+	 * BSSH_NM	업소명
+	 * PRDLST_NM	품목명
+	 * NTK_MTHD	섭취방법
+	 * RAWMTRL_NM	원재료
+	 * POG_DAYCNT	유통기한
+	 * PRIMARY_FNCLTY	주된기능성
+	 * CSTDY_MTHD	보관방법
+	 * IFTKN_ATNT_MATR_CN	섭취시주의사항
+	 * STDR_STND	기준규격
+	 * DISPOS	성상
+	 * SHAP	형태
+	 */
+	
+	private int LCNS_NO;
+	private int PRMS_DT;
+	private int PRDLST_REPORT_NO;
+	private int CRET_DTM;
+	private int LAST_UPDT_DTM;
+	private String BSSH_NM;
+	private String PRDLST_NM;
+	private String NTK_MTHD;
+	private String RAWMTRL_NM;
+	private String POG_DAYCNT;
+	private String PRIMARY_FNCLTY;
+	private String CSTDY_MTHD;
+	private String IFTKN_ATNT_MATR_CN;
+	private String STDR_STND;
+	private String DISPOS;
+	private String SHAP;
+	
+	public int getLCNS_NO() {
+		return LCNS_NO;
+	}
+	public void setLCNS_NO(int lCNS_NO) {
+		LCNS_NO = lCNS_NO;
+	}
+	public int getPRMS_DT() {
+		return PRMS_DT;
+	}
+	public void setPRMS_DT(int pRMS_DT) {
+		PRMS_DT = pRMS_DT;
+	}
+	public int getPRDLST_REPORT_NO() {
+		return PRDLST_REPORT_NO;
+	}
+	public void setPRDLST_REPORT_NO(int pRDLST_REPORT_NO) {
+		PRDLST_REPORT_NO = pRDLST_REPORT_NO;
+	}
+	public int getCRET_DTM() {
+		return CRET_DTM;
+	}
+	public void setCRET_DTM(int cRET_DTM) {
+		CRET_DTM = cRET_DTM;
+	}
+	public int getLAST_UPDT_DTM() {
+		return LAST_UPDT_DTM;
+	}
+	public void setLAST_UPDT_DTM(int lAST_UPDT_DTM) {
+		LAST_UPDT_DTM = lAST_UPDT_DTM;
+	}
+	public String getBSSH_NM() {
+		return BSSH_NM;
+	}
+	public void setBSSH_NM(String bSSH_NM) {
+		BSSH_NM = bSSH_NM;
+	}
+	public String getPRDLST_NM() {
+		return PRDLST_NM;
+	}
+	public void setPRDLST_NM(String pRDLST_NM) {
+		PRDLST_NM = pRDLST_NM;
+	}
+	public String getNTK_MTHD() {
+		return NTK_MTHD;
+	}
+	public void setNTK_MTHD(String nTK_MTHD) {
+		NTK_MTHD = nTK_MTHD;
+	}
+	public String getRAWMTRL_NM() {
+		return RAWMTRL_NM;
+	}
+	public void setRAWMTRL_NM(String rAWMTRL_NM) {
+		RAWMTRL_NM = rAWMTRL_NM;
+	}
+	public String getPOG_DAYCNT() {
+		return POG_DAYCNT;
+	}
+	public void setPOG_DAYCNT(String pOG_DAYCNT) {
+		POG_DAYCNT = pOG_DAYCNT;
+	}
+	public String getPRIMARY_FNCLTY() {
+		return PRIMARY_FNCLTY;
+	}
+	public void setPRIMARY_FNCLTY(String pRIMARY_FNCLTY) {
+		PRIMARY_FNCLTY = pRIMARY_FNCLTY;
+	}
+	public String getCSTDY_MTHD() {
+		return CSTDY_MTHD;
+	}
+	public void setCSTDY_MTHD(String cSTDY_MTHD) {
+		CSTDY_MTHD = cSTDY_MTHD;
+	}
+	public String getIFTKN_ATNT_MATR_CN() {
+		return IFTKN_ATNT_MATR_CN;
+	}
+	public void setIFTKN_ATNT_MATR_CN(String iFTKN_ATNT_MATR_CN) {
+		IFTKN_ATNT_MATR_CN = iFTKN_ATNT_MATR_CN;
+	}
+	public String getSTDR_STND() {
+		return STDR_STND;
+	}
+	public void setSTDR_STND(String sTDR_STND) {
+		STDR_STND = sTDR_STND;
+	}
+	public String getDISPOS() {
+		return DISPOS;
+	}
+	public void setDISPOS(String dISPOS) {
+		DISPOS = dISPOS;
+	}
+	public String getSHAP() {
+		return SHAP;
+	}
+	public void setSHAP(String sHAP) {
+		SHAP = sHAP;
+	}
+	
+	
+}
